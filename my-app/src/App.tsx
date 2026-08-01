ec883165d6560245a767f94f56fdfe74d4045c24
Helena Afshari <afshari.hln@gmail.com>
2026-08-01 12:16:52 +0330

    create function user

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,39 +1,66 @@
-import React from 'react';
+// import React from 'react';
 
-type user2 = {name:String; age:number}
-type array2 = Number[]
-export type array4 = (number|string)[]
-function App() {
+// // variables
+// type user2 = {name:String; age:number}
+// type array2 = Number[]
+// export type array4 = (number|string)[]
+// function App() {
   
-  let name : String = "ahmad"
-  let counter =1
-  let flag = false
-  let array =[1,"fds",false]
-  // یعنی این ارایه نامبر هست جنسش 
-  let array2:array2 = [1,2]
-  let array3: String[] = ["d","g"]
-  // حالا ترکیبی باشه تایپش هم عدد و هم استرینگ
-  let array4:array4 =[1,"rsfs"]
-  // تعریف آبجکت هست با {}
-  let user = {
-    name:"hanna",
-    age:24,
-    color:"red",
-  }
-  // جیا بگیره و چه تایپی باشه داخلش 
-  // محدودش کردیم که چی میتونیم بگیره نام و سن محدود شده
-   let user2: user2= {
-    name:"jfmm",
-    age:25
-  }
+//   let name : String = "ahmad"
+//   let counter =1
+//   let flag = false
+//   let array =[1,"fds",false]
+//   // یعنی این ارایه نامبر هست جنسش 
+//   let array2:array2 = [1,2]
+//   let array3: String[] = ["d","g"]
+//   // حالا ترکیبی باشه تایپش هم عدد و هم استرینگ
+//   let array4:array4 =[1,"rsfs"]
+//   // تعریف آبجکت هست با {}
+//   let user = {
+//     name:"hanna",
+//     age:24,
+//     color:"red",
+//   }
+//   // جیا بگیره و چه تایپی باشه داخلش 
+//   // محدودش کردیم که چی میتونیم بگیره نام و سن محدود شده
+//    let user2: user2= {
+//     name:"jfmm",
+//     age:25
+//   }
 
 
+//   return (
+
+//     <div className="App">
+//        <h1>typeScript</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import React from 'react';
+import User from './component/User'
+// بعد از دو نقطه خروجی رو مشخص میکنیم که نامبر هست
+function sum(a:number,b:number):number{
+  return a+b;
+}
+
+function App() {
   return (
 
     <div className="App">
        <h1>typeScript</h1>
+       <h2>{sum(5,5)}</h2>
+
+       <User name="hanna" age={22} hasChild={true}/>
+
     </div>
   );
 }
 
 export default App;
+
